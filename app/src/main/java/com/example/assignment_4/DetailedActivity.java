3e859440cb910b155f46d1688934b07627bab774
DELL <riyachow.9999@gmail.com>
2024-11-17 22:00:24 +0800

    2nd commit

---
+++ app/src/main/java/com/example/assignment_4/DetailedActivity.java
@@ -0,0 +1,37 @@
+package com.example.assignment_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.customlistview.databinding.ActivityDetailedBinding;
+
+public class DetailedActivity extends AppCompatActivity {
+
+    View binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View ActivityDetailedBinding = null;
+        binding = ActivityDetailedBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent intent = this.getIntent();
+        if (intent != null){
+            String name = intent.getStringExtra("name");
+            String time = intent.getStringExtra("time");
+            int ingredients = intent.getIntExtra("ingredients", R.string.maggiIngredients);
+            int desc = intent.getIntExtra("desc", R.string.maggieDesc);
+            int image = intent.getIntExtra("image", R.drawable.maggi);
+
+            binding.detailName.setText(name);
+            binding.detailTime.setText(time);
+            binding.detailDesc.setText(desc);
+            binding.detailIngredients.setText(ingredients);
+            binding.detailImage.setImageResource(image);
+        }
+    }
+}
